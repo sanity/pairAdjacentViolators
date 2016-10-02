4a5e571092be6c9b1b2ba521a188c32f51061b59
Ian Clarke <github@ian.33mail.com>
2016-10-02 08:36:54 -0500

    correct reference

--- src/test/kotlin/com/trystacks/pav/PAVSpec.kt
+++ src/test/kotlin/com/trystacks/pav/PAVSpec.kt
@@ -24,35 +24,35 @@ class PAVSpec : FreeSpec() {
             "should sort points by x value" {
                 val increasingPointsSorted = listOf(Point(3.0, 1.0), Point(4.0, 2.0), Point(5.0, 3.0))
                 val increasingPointsUnSorted = listOf(Point(5.0, 3.0), Point(3.0, 1.0), Point(4.0, 2.0))
-                PairAdjacentViolators(increasingPointsUnSorted).get() shouldEqual increasingPointsSorted
+                PairAdjacentViolators(increasingPointsUnSorted).isotonicPoints shouldEqual increasingPointsSorted
             }
 
             "should 'backtrack' to merge previous points where necessary" {
                 val points = listOf(Point(1.0, 5.0), Point(2.0, 6.0), Point(3.0, 1.0))
-                PairAdjacentViolators(points).get() shouldEqual listOf(Point(2.0, (5.0 + 6.0 + 1.0) / 3.0, 3.0))
+                PairAdjacentViolators(points).isotonicPoints shouldEqual listOf(Point(2.0, (5.0 + 6.0 + 1.0) / 3.0, 3.0))
             }
 
             "in ascending mode" - {
                 "should not change an already increasing set of points" {
                     val increasingPoints = listOf(Point(3.0, 1.0), Point(4.0, 2.0), Point(5.0, 3.0))
-                    PairAdjacentViolators(increasingPoints).get() shouldEqual increasingPoints
+                    PairAdjacentViolators(increasingPoints).isotonicPoints shouldEqual increasingPoints
                 }
 
                 "should merge a single pair of non-increasing points" {
                     val nonIncreasingPoints = listOf(Point(1.0, 2.0), Point(2.0, 1.0), Point(3.0, 5.0))
-                    PairAdjacentViolators(nonIncreasingPoints).get() shouldEqual listOf(Point(1.5, 1.5, 2.0), Point(3.0, 5.0))
+                    PairAdjacentViolators(nonIncreasingPoints).isotonicPoints shouldEqual listOf(Point(1.5, 1.5, 2.0), Point(3.0, 5.0))
                 }
 
                 "should merge multiple non-increasing points" {
                     val nonIncreasingPoints = listOf(Point(1.0, 5.0), Point(2.0, 4.0), Point(3.0, 3.0))
-                    PairAdjacentViolators(nonIncreasingPoints).get() shouldEqual listOf(Point(2.0, 4.0, 3.0))
+                    PairAdjacentViolators(nonIncreasingPoints).isotonicPoints shouldEqual listOf(Point(2.0, 4.0, 3.0))
                 }
             }
 
             "in descending mode" - {
                 "should not merge decreasing points" {
                     val decreasingPoints = listOf(Point(1.0, 5.0), Point(2.0, 4.0), Point(3.0, 3.0))
-                    PairAdjacentViolators(decreasingPoints, DECREASING).get() shouldEqual decreasingPoints
+                    PairAdjacentViolators(decreasingPoints, DECREASING).isotonicPoints shouldEqual decreasingPoints
                 }
             }
 
@@ -60,7 +60,7 @@ class PAVSpec : FreeSpec() {
                 "should interpolate as expected" {
                     val increasingPoints = listOf(Point(3.0, 1.0), Point(4.0, 2.0), Point(5.0, 3.0), Point(8.0, 4.0))
                     val pav = PairAdjacentViolators(increasingPoints)
-                    pav.get() shouldEqual increasingPoints
+                    pav.isotonicPoints shouldEqual increasingPoints
                     val splineInterpolator = pav.interpolator(PairAdjacentViolators.InterpolationStrategy.SPLINE)
                     var lastPointVar: Point? = null
                     for (point in increasingPoints) {
@@ -77,7 +77,7 @@ class PAVSpec : FreeSpec() {
                 "should reverse-interpolate as expected" {
                     val increasingPoints = listOf(Point(3.0, 1.0), Point(4.0, 2.0), Point(5.0, 3.0), Point(8.0, 4.0))
                     val pav = PairAdjacentViolators(increasingPoints)
-                    pav.get() shouldEqual increasingPoints
+                    pav.isotonicPoints shouldEqual increasingPoints
                     val splineInterpolator = pav.inverseInterpolator(PairAdjacentViolators.InterpolationStrategy.SPLINE)
                     var lastPointVar: Point? = null
                     for (point in increasingPoints) {
