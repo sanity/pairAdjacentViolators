156b758834b5de00223ecd08353003e85bd67db1
Ian Clarke <github@ian.33mail.com>
2016-12-20 10:17:43 -0600

    doh, these tests were moved but not added

---
+++ src/test/kotlin/com/github/sanity/pav/spline/CubicHermiteSplineSpec.kt
@@ -0,0 +1,27 @@
+package com.github.sanity.pav.spline
+
+import com.github.sanity.pav.spline.CubicHermiteSpline
+import io.kotlintest.specs.FreeSpec
+
+/**
+ * Created by ian on 12/7/16.
+ */
+class CubicHermiteSplineSpec : FreeSpec() {
+    init {
+        val chs = CubicHermiteSpline(1.5, 1.3, 0.2, 3.2, 3.4, 0.3)
+        "Spline should intersect (x1,y1)" {
+            chs.interpolate(1.5) shouldBe exactly(1.3)
+        }
+        "Spline should intersect (x2,y2)" {
+            chs.interpolate(3.2) shouldBe exactly(3.4)
+        }
+        "Spline slope at x1,y1 should be approximately 0.2" {
+            val slope = (chs.interpolate(1.51)-chs.interpolate(1.5)) / 0.01
+            slope shouldBe (0.2 plusOrMinus 0.05)
+        }
+        "Spline slope at x2,y2 should be approximately 0.3" {
+            val slope = (chs.interpolate(3.21)-chs.interpolate(3.2)) / 0.01
+            slope shouldBe (0.3 plusOrMinus 0.05)
+        }
+    }
+}
