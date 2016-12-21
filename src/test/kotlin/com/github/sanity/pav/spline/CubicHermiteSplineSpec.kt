7ba571d37395a49d92f530190a55f9f253202e09
Ian Clarke <github@ian.33mail.com>
2016-12-21 04:57:41 -0600

    minor unit test cleanup

--- src/test/kotlin/com/github/sanity/pav/spline/CubicHermiteSplineSpec.kt
+++ src/test/kotlin/com/github/sanity/pav/spline/CubicHermiteSplineSpec.kt
@@ -1,6 +1,5 @@
 package com.github.sanity.pav.spline
 
-import com.github.sanity.pav.spline.CubicHermiteSpline
 import io.kotlintest.specs.FreeSpec
 
 /**
@@ -8,20 +7,26 @@ import io.kotlintest.specs.FreeSpec
  */
 class CubicHermiteSplineSpec : FreeSpec() {
     init {
-        val chs = CubicHermiteSpline(1.5, 1.3, 0.2, 3.2, 3.4, 0.3)
+        val x1 = 1.5
+        val y1 = 1.3
+        val m1 = 0.2
+        val x2 = 3.2
+        val y2 = 3.4
+        val m2 = 0.3
+        val chs = CubicHermiteSpline(x1, y1, m1, x2, y2, m2)
         "Spline should intersect (x1,y1)" {
-            chs.interpolate(1.5) shouldBe exactly(1.3)
+            chs.interpolate(x1) shouldBe exactly(y1)
         }
         "Spline should intersect (x2,y2)" {
-            chs.interpolate(3.2) shouldBe exactly(3.4)
+            chs.interpolate(x2) shouldBe exactly(y2)
         }
-        "Spline slope at x1,y1 should be approximately 0.2" {
-            val slope = (chs.interpolate(1.51)-chs.interpolate(1.5)) / 0.01
-            slope shouldBe (0.2 plusOrMinus 0.05)
+        "Spline slope at x1,y1 should be approximately m1" {
+            val slope = (chs.interpolate(1.51)-chs.interpolate(x1)) / 0.01
+            slope shouldBe (m1 plusOrMinus 0.05)
         }
-        "Spline slope at x2,y2 should be approximately 0.3" {
-            val slope = (chs.interpolate(3.21)-chs.interpolate(3.2)) / 0.01
-            slope shouldBe (0.3 plusOrMinus 0.05)
+        "Spline slope at x2,y2 should be approximately m2" {
+            val slope = (chs.interpolate(3.21)-chs.interpolate(x2)) / 0.01
+            slope shouldBe (m2 plusOrMinus 0.05)
         }
     }
 }
