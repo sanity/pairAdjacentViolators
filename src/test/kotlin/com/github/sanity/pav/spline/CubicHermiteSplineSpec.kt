807e53f52110774e7ec69f1d47f76dca1c6cee93
Ian Clarke <sanity@users.noreply.github.com>
2016-12-30 12:21:15 -0600

    Update CubicHermiteSplineSpec.kt

--- src/test/kotlin/com/github/sanity/pav/spline/CubicHermiteSplineSpec.kt
+++ src/test/kotlin/com/github/sanity/pav/spline/CubicHermiteSplineSpec.kt
@@ -20,12 +20,13 @@ class CubicHermiteSplineSpec : FreeSpec() {
         "Spline should intersect (x2,y2)" {
             chs.interpolate(x2) shouldBe exactly(y2)
         }
+        val delta = 0.01
         "Spline slope at x1,y1 should be approximately m1" {
-            val slope = (chs.interpolate(1.51)-chs.interpolate(x1)) / 0.01
+            val slope = (chs.interpolate(x1 + delta)-chs.interpolate(x1)) / delta
             slope shouldBe (m1 plusOrMinus 0.05)
         }
         "Spline slope at x2,y2 should be approximately m2" {
-            val slope = (chs.interpolate(3.21)-chs.interpolate(x2)) / 0.01
+            val slope = (chs.interpolate(x2 + delta)-chs.interpolate(x2)) / delta
             slope shouldBe (m2 plusOrMinus 0.05)
         }
     }
